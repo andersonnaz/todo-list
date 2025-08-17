fc957333ca95cd37bd9b341bce740e3bb8ab50e6
Anderson Naz <anderson_1004@hotmail.com>
2025-08-17 20:18:33 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Text from "./components/text";
+
+export default function App(){
+  return(
+    <div className="flex flex-col gap-2">
+      <Text variant="body-sm-bold" className="text-pink-base">
+        Olá Mundo!
+      </Text>
+      <Text className="text-green-base">
+        Olá Mundo!
+      </Text>
+      <Text variant="body-md-bold" className="text-pink-base">
+        Olá Mundo!
+      </Text>
+    </div>
+  )
+}
